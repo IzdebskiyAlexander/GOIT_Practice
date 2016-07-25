db0771d5e6d0bd67a27ee682da51827e975c64db
IzdebskiyAlexander <a.izdebskiy@gen.tech>
2016-07-25 20:07:46 +0300

    second

---
+++ src/test/java/ParallelTestTry.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+
+public class ParallelTestTry extends BrowserFactory{
+
+    @Test
+    void tryIt() throws InterruptedException {
+        driver.get("https://kismia.com/");
+        Thread.sleep(5000);
+    }
+
+
+
+}
